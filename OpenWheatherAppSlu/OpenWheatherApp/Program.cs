8217c1fe6065b73ce7392f88bccb1c29531df6b5
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-10-07 10:55:51 +0200

    UPLOADED

--- OpenWheatherAppSlu/OpenWheatherApp/Program.cs
+++ OpenWheatherAppSlu/OpenWheatherApp/Program.cs
@@ -14,22 +14,27 @@ namespace OpenWeatherApp
             
             var dman = await WeatherGenerator.WeatherDataAsync();
 
-
-            /*Console.BackgroundColor = ConsoleColor.Blue;*/
+            /* TO CHANGE BACKGROUND COLOR */
+            /* Console.BackgroundColor = ConsoleColor.Blue; */
 
             string capeTown = "FORCAST FOR CAPE TOWN";
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (capeTown.Length / 2)) + "}", capeTown));         
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (capeTown.Length / 2)) + "}", capeTown));
+            Console.WriteLine();
            
             Console.WriteLine("TEMPERATURE: "+dman.Temperature);
             Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
 
             Console.WriteLine("HUMIDITY: "+dman.Humidity);
             Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
 
             Console.WriteLine("WEATHER: "+dman.Weather);
             Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
 
+            /*TO CENTER YOUR TEXT*/
             string tag = "$ 😎 D-M4N 😎 $";
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (tag.Length / 2)) + "}", tag));
